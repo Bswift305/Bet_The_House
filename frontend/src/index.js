34aabe0c771aefb7be506db4c8e0201e4c84f5f7
Darrel Copper <darrel.copper29@gmail.com>
2025-07-23 14:08:34 -0400

    Fix syntax error in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import './index.css'; // your Tailwind CSS
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
