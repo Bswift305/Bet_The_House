6a38fe87e84d958d7ed959013c0eaf28b5dd2df2
Bswift305 <darrel.copper29@gmail.com>
2025-08-09 20:40:59 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true
+  // If you load remote images, add domains:
+  // images: { domains: ["example.com"] },
+  // If you use experimental features, enable here:
+  // experimental: { typedRoutes: true },
 };
 
 export default nextConfig;
+
